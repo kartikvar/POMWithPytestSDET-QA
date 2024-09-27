e1b1e28752a0e384f91e93c6bda68da6e80559d7
Kartik <kartikvar@gmail.com>
2024-09-27 22:03:34 +0530

    first commit

---
+++ testCases/conftest.py
@@ -0,0 +1,15 @@
+import os
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+@pytest.fixture()
+def setup():
+    os.environ['WDM_SSL_VERIFY'] = '0'
+    service = Service()
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
---
+++ testCases/test_login.py
@@ -0,0 +1,40 @@
+import os
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from pageObjects.LoginPage import LoginPage
+
+
+class Test_001_Login:
+    baseURL = "https://admin-demo.nopcommerce.com/"
+    username = "admin@yourstore.com"
+    password = "admin"
+
+    def test_homepage_title(self, setup):
+        self.driver = setup
+        self.driver.maximize_window()
+        self.driver.get(self.baseURL)
+        actual_title = self.driver.title
+        self.driver.close()
+        if actual_title == "Your store. Login":
+            assert True
+        else:
+            assert False
+
+    def test_login(self, setup):
+        self.driver = setup
+        self.driver.maximize_window()
+        self.driver.get(self.baseURL)
+        self.lp = LoginPage(self.driver)
+        self.lp.set_username(self.username)
+        self.lp.set_password(self.password)
+        self.lp.click_login()
+        actual_title = self.driver.title
+        if actual_title == "Dashboard / nopCommerce administration":
+            assert True
+            self.driver.close()
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_login.png")
+            self.driver.close()
+            assert False
